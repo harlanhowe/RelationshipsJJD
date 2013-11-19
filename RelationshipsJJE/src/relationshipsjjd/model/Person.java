3b89b28a161c360b9a995d5b68d5329bb130c657
joshua.kassab <joshua.kassab@172.16.221.85>
2013-11-19 11:23:31 -0600

    Updated Person

--- RelationshipsJJE/src/relationshipsjjd/model/Person.java
+++ RelationshipsJJE/src/relationshipsjjd/model/Person.java
@@ -4,10 +4,55 @@
  */
 package relationshipsjjd.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author joshua.kassab
  */
-public class Person {
+public class Person 
+{
+    private String name;
+    private ArrayList<Relationship> relationships;
+    private int ID;
+    boolean isMale;
     
+    /***
+     * Constructor.  Is given a person's name and ID
+     * @param personName
+     * @param personID 
+     */
+    public Person(String personName, int personID)
+    {
+        name=personName;
+        ID=personID;
+    }
+    
+    /***
+     * Returns a list of relationships pertaining to this person
+     * @return 
+     */
+    public ArrayList<Relationship> getRelations()
+    {
+        return relationships;
+    }
+    
+    /***
+     * Returns this person's ID
+     * @return 
+     */
+    public int getID()
+    {
+        return ID;
+    }
+    
+    /***
+     * Returns one string that is made up of all of this person's relationships
+     * @return 
+     */
+    @Override
+    public String toString()
+    {
+        return "";
+    }
 }
